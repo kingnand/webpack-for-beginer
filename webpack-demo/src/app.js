c3d39f25c1a45cc31204bfd3c24a2130cbd9b2ea
kingnand <kingnand.90@gmail.com>
2017-06-05 23:29:24 +0700

    Example: multi js and css

--- webpack-demo/src/app.js
+++ webpack-demo/src/app.js
@@ -1,26 +1,8 @@
 // src/app.js
+import './style.scss'
 import {groupBy} from 'lodash/collection'
-import people2 from './people'
+import people from './people'
 
-const people = [{
-  manager: 'Jen',
-  name: 'Bob'
-}, {
-  manager: 'Jen',
-  name: 'Sue'
-}, {
-  manager: 'Bob',
-  name: 'Shirley'
-}, {
-  manager: 'Bob',
-  name: 'Terrence'
-}]
 const managerGroups = groupBy(people, 'manager')
-
 const root = document.querySelector('#root')
 root.innerHTML = `<p>Inline data</p><pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
-
-
-const managerGroups2 = groupBy(people2, 'manager')
-const root2 = document.querySelector('#root2')
-root2.innerHTML = `<p>Module data</p><pre>${JSON.stringify(managerGroups2, null, 2)}</pre>`
