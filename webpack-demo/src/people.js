da2ab681d6fca0ad5ab66ae31bff98c39c00f4ad
kingnand <kingnand.90@gmail.com>
2017-06-05 22:55:31 +0700

    Start using webpack with simple example

---
+++ webpack-demo/src/people.js
@@ -0,0 +1,15 @@
+const people = [{
+  manager: 'Je2',
+  name: 'Bob'
+}, {
+  manager: 'Je2',
+  name: 'Sue'
+}, {
+  manager: 'Bo2',
+  name: 'Shirley'
+}, {
+  manager: 'Bob',
+  name: 'Terrence'
+}]
+
+export default people
